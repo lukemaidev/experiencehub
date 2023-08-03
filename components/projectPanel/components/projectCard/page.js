74044c6e729257d062e6e790dd154ea2fe8c0a9c
Luke Mai <n11373661@qut.edu.au>
2023-08-03 12:46:27 +1000

    Adding project panel

--- components/projectPanel/components/projectCard/page.js
+++ components/projectPanel/components/projectCard/page.js
@@ -1,7 +1,15 @@
+const testingSkills = ["1","2","3"]
+
 export default function ProjectCard(props){
     return(
         <div>
             {props.name}
+            {testingSkills.map((value,index) =>
+            {
+                return(<div key={"project-card-skills"+index}>
+                    {value}
+                </div>)
+            })}
         </div>
     )
 }
--- components/projectPanel/page.js
+++ components/projectPanel/page.js
@@ -1,9 +1,13 @@
 import ProjectCard from "./components/projectCard/page";
 
+const projectCardNames = ["Something", "Something eklse", "Soemthing else else"]
+
 export default function ProjectPanel(){
     return (
         <div>
-            <ProjectCard name="Something"/>
+            {projectCardNames.map((value, index) => {
+                return(<ProjectCard key={"project-card-index-"+index} name={value}/>)
+            })}
         </div>
     )
 }
